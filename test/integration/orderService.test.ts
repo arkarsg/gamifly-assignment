ecfd4da655dfffc14060efedce1bc54fbe19391f
arkarsg <alxarkar@gmail.com>
2024-10-11 03:11:14 +0800

    fix(test): avoid truthy test

--- test/integration/orderService.test.ts
+++ test/integration/orderService.test.ts
@@ -8,7 +8,7 @@ describe("Order Service", () => {
 
   beforeAll(async () => {
     testOrder = {
-      price: "100.00000",
+      price: "100.00",
       currency: CurrencyType.USD,
       customerName: "Test Customer",
     };
@@ -17,10 +17,9 @@ describe("Order Service", () => {
   test("createOrder should insert a new order", async () => {
     const newOrder = await createOrder(testOrder);
 
-    // Expect the new order to be defined
     expect(newOrder).toBeDefined();
     if (newOrder) {
-      expect(newOrder.price).toBe(testOrder.price);
+      expect(newOrder.price).toBe("100.00000");
       expect(newOrder.currency).toBe(testOrder.currency);
       expect(newOrder.customerName).toBe(testOrder.customerName);
     }
@@ -39,11 +38,10 @@ describe("Order Service", () => {
   test("getAllOrders should return all orders", async () => {
     const orders = await getAllOrders();
 
-    // Expect the list of orders to include the previously created order
     expect(orders.length).toBeGreaterThan(0);
     const lastOrder = orders[orders.length - 1];
 
-    expect(lastOrder.price).toBe(testOrder.price);
+    expect(lastOrder.price).toBe("100.00000");
     expect(lastOrder.currency).toBe(testOrder.currency);
     expect(lastOrder.customerName).toBe(testOrder.customerName);
   });
